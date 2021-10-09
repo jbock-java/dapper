ee59acea3cfad6016aacb66ea63ccff548759019
jbock <jbock-java@gmx.de>
2021-10-09 21:28:17 +0200

    INIT: methods in KotlinMetadataUtil return false
    28 errors remaining

--- compiler/src/main/java/dagger/internal/codegen/kotlin/KotlinMetadataUtil.java
+++ compiler/src/main/java/dagger/internal/codegen/kotlin/KotlinMetadataUtil.java
@@ -16,19 +16,10 @@
 
 package dagger.internal.codegen.kotlin;
 
-import static com.google.auto.common.AnnotationMirrors.getAnnotatedAnnotations;
-import static com.google.auto.common.MoreElements.isAnnotationPresent;
-import static dagger.internal.codegen.langmodel.DaggerElements.closestEnclosingTypeElement;
-import static kotlinx.metadata.Flag.Class.IS_COMPANION_OBJECT;
-import static kotlinx.metadata.Flag.Class.IS_DATA;
-import static kotlinx.metadata.Flag.Class.IS_OBJECT;
-import static kotlinx.metadata.Flag.IS_PRIVATE;
-
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import dagger.internal.codegen.extension.DaggerCollectors;
-import java.lang.annotation.Annotation;
-import java.util.Optional;
+
 import javax.inject.Inject;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
@@ -36,9 +27,9 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.ElementFilter;
-import kotlin.Metadata;
-import kotlin.jvm.JvmStatic;
-import kotlinx.metadata.Flag;
+import java.lang.annotation.Annotation;
+
+import static com.google.auto.common.AnnotationMirrors.getAnnotatedAnnotations;
 
 /** Utility class for interacting with Kotlin Metadata. */
 public final class KotlinMetadataUtil {
@@ -55,7 +46,7 @@ public final class KotlinMetadataUtil {
    * an element that does.
    */
   public boolean hasMetadata(Element element) {
-    return isAnnotationPresent(closestEnclosingTypeElement(element), Metadata.class);
+    return false;
   }
 
   /**
@@ -84,20 +75,17 @@ public final class KotlinMetadataUtil {
 
   /** Returns {@code true} if this type element is a Kotlin Object. */
   public boolean isObjectClass(TypeElement typeElement) {
-    return hasMetadata(typeElement)
-        && metadataFactory.create(typeElement).classMetadata().flags(IS_OBJECT);
+    return false;
   }
 
   /** Returns {@code true} if this type element is a Kotlin data class. */
   public boolean isDataClass(TypeElement typeElement) {
-    return hasMetadata(typeElement)
-        && metadataFactory.create(typeElement).classMetadata().flags(IS_DATA);
+    return false;
   }
 
   /* Returns {@code true} if this type element is a Kotlin Companion Object. */
   public boolean isCompanionObjectClass(TypeElement typeElement) {
-    return hasMetadata(typeElement)
-        && metadataFactory.create(typeElement).classMetadata().flags(IS_COMPANION_OBJECT);
+    return false;
   }
 
   /** Returns {@code true} if this type element is a Kotlin object or companion object. */
@@ -131,41 +119,13 @@ public final class KotlinMetadataUtil {
    * source.
    */
   public boolean isVisibilityPrivate(TypeElement typeElement) {
-    return hasMetadata(typeElement)
-        && metadataFactory.create(typeElement).classMetadata().flags(IS_PRIVATE);
-  }
-
-  /**
-   * Returns {@code true} if the given type element was declared {@code internal} in its Kotlin
-   * source.
-   */
-  public boolean isVisibilityInternal(TypeElement type) {
-    return hasMetadata(type)
-        && metadataFactory.create(type).classMetadata().flags(Flag.IS_INTERNAL);
-  }
-
-  /**
-   * Returns {@code true} if the given executable element was declared {@code internal} in its
-   * Kotlin source.
-   */
-  public boolean isVisibilityInternal(ExecutableElement method) {
-    return hasMetadata(method)
-        && metadataFactory.create(method).getFunctionMetadata(method).flags(Flag.IS_INTERNAL);
-  }
-
-  public Optional<ExecutableElement> getPropertyGetter(VariableElement fieldElement) {
-    return metadataFactory.create(fieldElement).getPropertyGetter(fieldElement);
-  }
-
-  public boolean containsConstructorWithDefaultParam(TypeElement typeElement) {
-    return hasMetadata(typeElement)
-        && metadataFactory.create(typeElement).containsConstructorWithDefaultParam();
+    return false;
   }
 
   /**
    * Returns {@code true} if the <code>@JvmStatic</code> annotation is present in the given element.
    */
   public static boolean isJvmStaticPresent(ExecutableElement element) {
-    return isAnnotationPresent(element, JvmStatic.class);
+    return false;
   }
 }
