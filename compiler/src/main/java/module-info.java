7b0bcb2c3016c2eb834ba0af709c378befba1fac
jbock <jbock-java@gmx.de>
2021-11-18 09:33:02 +0100

    add module info

---
+++ compiler/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module dagger.compiler {
+  requires dagger;
+  requires java.compiler;
+  requires java.logging;
+  requires com.google.common;
+  requires com.google.auto.value;
+  requires auto.common;
+  requires com.squareup.javapoet;
+}
