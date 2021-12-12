63f0e28b51509a62b1ed681562acbd6b842b93d2
jbock <jbock-java@gmx.de>
2021-12-12 16:35:59 +0100

    remove dependency on AutoValue

--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module dagger.compiler {
   requires java.compiler;
   requires java.logging;
   requires com.google.common;
-  requires com.google.auto.value;
   requires auto.common;
   requires com.squareup.javapoet;
 }
