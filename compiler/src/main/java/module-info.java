fe4c77f650be667820b45a0e398a36a090b52bfa
jbock <jbock-java@gmx.de>
2021-12-25 14:43:32 +0100

    use guava-graph

--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module dagger.compiler {
   requires java.compiler;
   requires java.logging;
   requires com.google.common;
+  requires com.google.common.graph;
   requires auto.common;
   requires com.squareup.javapoet;
 }
