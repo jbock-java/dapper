08a0325e1355e2179f8b82cc89fd39ad609c8f25
jbock <jbock-java@gmx.de>
2021-12-28 14:46:22 +0100

    remove production tests

--- compiler/src/test/java/dagger/internal/codegen/ProductionGraphValidationTest.java
+++ compiler/src/test/java/dagger/internal/codegen/ProductionGraphValidationTest.java
@@ -16,61 +16,17 @@
 
 package dagger.internal.codegen;
 
-import static com.google.testing.compile.CompilationSubject.assertThat;
-import static dagger.internal.codegen.Compilers.compilerWithOptions;
-import static dagger.internal.codegen.Compilers.daggerCompiler;
-
 import com.google.testing.compile.Compilation;
 import com.google.testing.compile.JavaFileObjects;
-import javax.tools.JavaFileObject;
 import org.junit.jupiter.api.Test;
+
+import javax.tools.JavaFileObject;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+import static dagger.internal.codegen.Compilers.daggerCompiler;
 
 /** Producer-specific validation tests. */
 public class ProductionGraphValidationTest {
-  private static final JavaFileObject EXECUTOR_MODULE =
-      JavaFileObjects.forSourceLines(
-          "test.ExecutorModule",
-          "package test;",
-          "",
-          "import com.google.common.util.concurrent.MoreExecutors;",
-          "import dagger.Module;",
-          "import dagger.Provides;",
-          "import dagger.producers.Production;",
-          "import java.util.concurrent.Executor;",
-          "",
-          "@Module",
-          "class ExecutorModule {",
-          "  @Provides @Production Executor executor() {",
-          "    return MoreExecutors.directExecutor();",
-          "  }",
-          "}");
-
-  @Test
-  public void componentProductionWithNoDependencyChain() {
-    JavaFileObject component = JavaFileObjects.forSourceLines("test.TestClass",
-        "package test;",
-        "",
-        "import com.google.common.util.concurrent.ListenableFuture;",
-        "import dagger.producers.ProductionComponent;",
-        "",
-        "final class TestClass {",
-        "  interface A {}",
-        "",
-        "  @ProductionComponent(modules = ExecutorModule.class)",
-        "  interface AComponent {",
-        "    ListenableFuture<A> getA();",
-        "  }",
-        "}");
-
-    Compilation compilation = daggerCompiler().compile(EXECUTOR_MODULE, component);
-    assertThat(compilation).failed();
-    assertThat(compilation)
-        .hadErrorContaining(
-            "TestClass.A cannot be provided without an @Provides- or @Produces-annotated "
-                + "method.")
-        .inFile(component)
-        .onLineContaining("interface AComponent");
-  }
 
   @Test
   public void componentWithBadModule() {
