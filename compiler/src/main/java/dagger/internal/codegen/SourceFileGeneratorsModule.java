912bb538071904eac6da51d9472e4217140a482c
jbock <jbock-java@gmx.de>
2021-12-28 09:11:07 +0100

    remove HjarSourceFileGenerator

--- compiler/src/main/java/dagger/internal/codegen/SourceFileGeneratorsModule.java
+++ compiler/src/main/java/dagger/internal/codegen/SourceFileGeneratorsModule.java
@@ -16,14 +16,12 @@
 
 package dagger.internal.codegen;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 import dagger.internal.codegen.base.SourceFileGenerator;
 import dagger.internal.codegen.binding.MembersInjectionBinding;
 import dagger.internal.codegen.binding.ProvisionBinding;
-import dagger.internal.codegen.compileroption.CompilerOptions;
 import dagger.internal.codegen.writing.FactoryGenerator;
-import dagger.internal.codegen.writing.HjarSourceFileGenerator;
 import dagger.internal.codegen.writing.MembersInjectorGenerator;
 import dagger.internal.codegen.writing.ModuleGenerator;
 import dagger.internal.codegen.writing.ModuleProxies.ModuleConstructorProxyGenerator;
@@ -31,31 +29,15 @@ import dagger.internal.codegen.writing.ModuleProxies.ModuleConstructorProxyGener
 import javax.lang.model.element.TypeElement;
 
 @Module
-abstract class SourceFileGeneratorsModule {
+interface SourceFileGeneratorsModule {
 
-  @Provides
-  static SourceFileGenerator<ProvisionBinding> factoryGenerator(
-      FactoryGenerator generator, CompilerOptions compilerOptions) {
-    return hjarWrapper(generator, compilerOptions);
-  }
+  @Binds
+  SourceFileGenerator<ProvisionBinding> factoryGenerator(FactoryGenerator generator);
 
-  @Provides
-  static SourceFileGenerator<MembersInjectionBinding> membersInjectorGenerator(
-      MembersInjectorGenerator generator, CompilerOptions compilerOptions) {
-    return hjarWrapper(generator, compilerOptions);
-  }
+  @Binds
+  SourceFileGenerator<MembersInjectionBinding> membersInjectorGenerator(MembersInjectorGenerator generator);
 
-  @Provides
+  @Binds
   @ModuleGenerator
-  static SourceFileGenerator<TypeElement> moduleConstructorProxyGenerator(
-      ModuleConstructorProxyGenerator generator, CompilerOptions compilerOptions) {
-    return hjarWrapper(generator, compilerOptions);
-  }
-
-  private static <T> SourceFileGenerator<T> hjarWrapper(
-      SourceFileGenerator<T> generator, CompilerOptions compilerOptions) {
-    return compilerOptions.headerCompilation()
-        ? HjarSourceFileGenerator.wrap(generator)
-        : generator;
-  }
+  SourceFileGenerator<TypeElement> moduleConstructorProxyGenerator(ModuleConstructorProxyGenerator generator);
 }
