1f77a99fe00d4a7657e01cfb0d050a351f8c0b15
jbock <jbock-java@gmx.de>
2022-02-14 19:23:14 +0100

    internal change

--- compiler/src/main/java/dagger/internal/codegen/validation/BindingGraphValidator.java
+++ compiler/src/main/java/dagger/internal/codegen/validation/BindingGraphValidator.java
@@ -16,37 +16,24 @@
 
 package dagger.internal.codegen.validation;
 
-import static java.util.Objects.requireNonNull;
-import static javax.tools.Diagnostic.Kind.ERROR;
-
 import dagger.internal.codegen.compileroption.CompilerOptions;
 import dagger.internal.codegen.compileroption.ValidationType;
-import dagger.internal.codegen.validation.DiagnosticReporterFactory.DiagnosticReporterImpl;
 import dagger.internal.codegen.xprocessing.XTypeElement;
 import dagger.model.BindingGraph;
-import dagger.spi.BindingGraphPlugin;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import java.util.Set;
-import javax.lang.model.element.TypeElement;
 
 /** Validates a {@link BindingGraph}. */
 @Singleton
 public final class BindingGraphValidator {
-  private final Set<BindingGraphPlugin> validationPlugins;
-  private final Set<BindingGraphPlugin> externalPlugins;
-  private final DiagnosticReporterFactory diagnosticReporterFactory;
+  private final ValidationBindingGraphPlugins validationPlugins;
   private final CompilerOptions compilerOptions;
 
   @Inject
   BindingGraphValidator(
-      @Validation Set<BindingGraphPlugin> validationPlugins,
-      Set<BindingGraphPlugin> externalPlugins,
-      DiagnosticReporterFactory diagnosticReporterFactory,
+      ValidationBindingGraphPlugins validationPlugins,
       CompilerOptions compilerOptions) {
     this.validationPlugins = validationPlugins;
-    this.externalPlugins = externalPlugins;
-    this.diagnosticReporterFactory = requireNonNull(diagnosticReporterFactory);
     this.compilerOptions = compilerOptions;
   }
 
@@ -62,47 +49,15 @@ public final class BindingGraphValidator {
 
   /** Returns {@code true} if no errors are reported for {@code graph}. */
   public boolean isValid(BindingGraph graph) {
-    return validate(graph) && visitPlugins(graph);
+    return visitValidationPlugins(graph);
   }
 
   /** Returns {@code true} if validation plugins report no errors. */
-  private boolean validate(BindingGraph graph) {
+  private boolean visitValidationPlugins(BindingGraph graph) {
     if (graph.isFullBindingGraph() && !requiresFullBindingGraphValidation()) {
       return true;
     }
 
-    boolean errorsAsWarnings =
-        graph.isFullBindingGraph()
-            && compilerOptions.fullBindingGraphValidationType().equals(ValidationType.WARNING);
-
-    return runPlugins(validationPlugins, graph, errorsAsWarnings);
-  }
-
-  /** Returns {@code true} if external plugins report no errors. */
-  private boolean visitPlugins(BindingGraph graph) {
-    TypeElement component = graph.rootComponentNode().componentPath().currentComponent().java();
-    if (graph.isFullBindingGraph()
-        // TODO(b/135938915): Consider not visiting plugins if only
-        // fullBindingGraphValidation is enabled.
-        && !requiresFullBindingGraphValidation()
-        && !compilerOptions.pluginsVisitFullBindingGraphs(component)) {
-      return true;
-    }
-    return runPlugins(externalPlugins, graph, /*errorsAsWarnings=*/ false);
-  }
-
-  /** Returns {@code false} if any of the plugins reported an error. */
-  private boolean runPlugins(
-      Set<BindingGraphPlugin> plugins, BindingGraph graph, boolean errorsAsWarnings) {
-    boolean isClean = true;
-    for (BindingGraphPlugin plugin : plugins) {
-      DiagnosticReporterImpl reporter =
-          diagnosticReporterFactory.reporter(graph, plugin.pluginName(), errorsAsWarnings);
-      plugin.visitGraph(graph, reporter);
-      if (reporter.reportedDiagnosticKinds().contains(ERROR)) {
-        isClean = false;
-      }
-    }
-    return isClean;
+    return validationPlugins.visit(graph);
   }
 }
