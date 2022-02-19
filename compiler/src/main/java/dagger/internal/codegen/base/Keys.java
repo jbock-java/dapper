32ad491693d9336580b0d5016a22343a226e2a4e
jbock <jbock-java@gmx.de>
2022-02-19 10:53:45 +0100

    pick a61aa50c8 Fix regression that allowed requesting a raw @Inject constructor type.
    see: https://github.com/google/dagger/commit/a61aa50c8dc007d293b9b63871cca7a3ab293573
    date: 16 Dec 2021

--- compiler/src/main/java/dagger/internal/codegen/base/Keys.java
+++ compiler/src/main/java/dagger/internal/codegen/base/Keys.java
@@ -18,11 +18,13 @@ package dagger.internal.codegen.base;
 
 import static dagger.internal.codegen.base.ComponentAnnotation.allComponentAndCreatorAnnotations;
 import static dagger.internal.codegen.langmodel.DaggerElements.isAnyAnnotationPresent;
+import static dagger.internal.codegen.xprocessing.XConverters.toJavac;
 import static io.jbock.auto.common.MoreTypes.asDeclared;
 import static io.jbock.auto.common.MoreTypes.asTypeElement;
 
 import dagger.internal.codegen.langmodel.DaggerTypes;
 import dagger.internal.codegen.xprocessing.XAnnotation;
+import dagger.internal.codegen.xprocessing.XConverters;
 import dagger.internal.codegen.xprocessing.XType;
 import dagger.spi.model.DaggerAnnotation;
 import dagger.spi.model.Key;
@@ -36,6 +38,11 @@ import javax.lang.model.type.TypeMirror;
 
 /** Utility methods related to {@link Key}s. */
 public final class Keys {
+  public static boolean isValidMembersInjectionKey(Key key) {
+    return !key.qualifier().isPresent()
+        && !key.multibindingContributionIdentifier().isPresent()
+        && key.type().java().getKind().equals(TypeKind.DECLARED);
+  }
 
   /**
    * Returns {@code true} if this is valid as an implicit key (that is, if it's valid for a
@@ -53,7 +60,7 @@ public final class Keys {
    */
   public static boolean isValidImplicitProvisionKey(
       Optional<XAnnotation> qualifier, XType type, DaggerTypes types) {
-    return isValidImplicitProvisionKey(qualifier.map(XAnnotation::toJavac), type.toJavac(), types);
+    return isValidImplicitProvisionKey(qualifier.map(XConverters::toJavac), toJavac(type), types);
   }
 
   /**
@@ -89,12 +96,17 @@ public final class Keys {
       }
     }
 
+    // The "definedType" is the type we get from the element itself, as opposed to the type the user
+    // declared. This "definedType" includes any type parameters that appear on the element, whereas
+    // the "declaredType" may contain resolved or raw types as declared by the user.
+    DeclaredType definedType = asDeclared(declaredType.asElement().asType());
+
     // Also validate that the key is not the erasure of a generic type.
     // If it is, that means the user referred to Foo<T> as just 'Foo',
     // which we don't allow.  (This is a judgement call -- we *could*
     // allow it and instantiate the type bounds... but we don't.)
-    return declaredType.getTypeArguments().isEmpty()
-        || !types.isSameType(types.erasure(declaredType), declaredType);
+    return definedType.getTypeArguments().isEmpty()
+        || !types.isSameType(types.erasure(definedType), declaredType);
   }
 
   /**
@@ -102,9 +114,9 @@ public final class Keys {
    * component/subcomponent.
    */
   public static boolean isComponentOrCreator(Key key) {
-    return key.qualifier().isEmpty()
+    return !key.qualifier().isPresent()
         && key.type().java().getKind() == TypeKind.DECLARED
         && isAnyAnnotationPresent(
-        asTypeElement(key.type().java()), allComponentAndCreatorAnnotations());
+            asTypeElement(key.type().java()), allComponentAndCreatorAnnotations());
   }
 }
---
+++ compiler/src/test/java/dagger/internal/codegen/RawTypeInjectionTest.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2021 The Dagger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dagger.internal.codegen;
+
+import static dagger.internal.codegen.Compilers.daggerCompiler;
+import static io.jbock.testing.compile.CompilationSubject.assertThat;
+
+import io.jbock.testing.compile.Compilation;
+import io.jbock.testing.compile.JavaFileObjects;
+import javax.tools.JavaFileObject;
+import org.junit.jupiter.api.Test;
+
+class RawTypeInjectionTest {
+  @Test
+  void rawEntryPointTest() {
+    JavaFileObject component =
+        JavaFileObjects.forSourceLines(
+            "test.TestComponent",
+            "package test;",
+            "",
+            "import dagger.Component;",
+            "",
+            "@Component",
+            "interface TestComponent {",
+            "  Foo foo();",  // Fail: requesting raw type
+            "}");
+    JavaFileObject foo =
+        JavaFileObjects.forSourceLines(
+            "test.Foo",
+            "package test;",
+            "",
+            "import jakarta.inject.Inject;",
+            "",
+            "class Foo<T> {",
+            "  @Inject Foo() {}",
+            "}");
+
+    Compilation compilation = daggerCompiler().compile(component, foo);
+    assertThat(compilation).failed();
+    assertThat(compilation)
+        .hadErrorContaining("Foo cannot be provided without a @Provides-annotated method.")
+        .inFile(component)
+        .onLine(6);
+  }
+
+  @Test
+  void rawProvidesRequestTest() {
+    JavaFileObject component =
+        JavaFileObjects.forSourceLines(
+            "test.TestComponent",
+            "package test;",
+            "",
+            "import dagger.Component;",
+            "",
+            "@Component(modules = TestModule.class)",
+            "interface TestComponent {",
+            "  int integer();",
+            "}");
+    JavaFileObject foo =
+        JavaFileObjects.forSourceLines(
+            "test.Foo",
+            "package test;",
+            "",
+            "import jakarta.inject.Inject;",
+            "",
+            "class Foo<T> {",
+            "  @Inject Foo() {}",
+            "}");
+    JavaFileObject module =
+        JavaFileObjects.forSourceLines(
+            "test.TestModule",
+            "package test;",
+            "",
+            "import dagger.Module;",
+            "import dagger.Provides;",
+            "",
+            "@Module",
+            "class TestModule {",
+            "  @Provides",
+            "  int provideFoo(Foo foo) {", // Fail: requesting raw type
+            "    return 0;",
+            "  }",
+            "}");
+
+
+    Compilation compilation = daggerCompiler().compile(component, foo, module);
+    assertThat(compilation).failed();
+    assertThat(compilation)
+        .hadErrorContaining("Foo cannot be provided without a @Provides-annotated method.")
+        .inFile(component)
+        .onLine(6);
+  }
+
+  @Test
+  void rawInjectConstructorRequestTest() {
+    JavaFileObject component =
+        JavaFileObjects.forSourceLines(
+            "test.TestComponent",
+            "package test;",
+            "",
+            "import dagger.Component;",
+            "",
+            "@Component",
+            "interface TestComponent {",
+            "  Foo foo();",
+            "}");
+    JavaFileObject foo =
+        JavaFileObjects.forSourceLines(
+            "test.Foo",
+            "package test;",
+            "",
+            "import jakarta.inject.Inject;",
+            "",
+            "class Foo<T> {",
+            "  @Inject Foo() {}",
+            "}");
+    JavaFileObject bar =
+        JavaFileObjects.forSourceLines(
+            "test.Bar",
+            "package test;",
+            "",
+            "import jakarta.inject.Inject;",
+            "",
+            "class Bar {",
+            "  @Inject Bar(Foo foo) {}", // Fail: requesting raw type
+            "}");
+
+
+    Compilation compilation = daggerCompiler().compile(component, foo, bar);
+    assertThat(compilation).failed();
+    assertThat(compilation)
+        .hadErrorContaining("Foo cannot be provided without a @Provides-annotated method.")
+        .inFile(component)
+        .onLine(6);
+  }
+
+  @Test
+  void rawProvidesReturnTest() {
+    JavaFileObject component =
+        JavaFileObjects.forSourceLines(
+            "test.TestComponent",
+            "package test;",
+            "",
+            "import dagger.Component;",
+            "",
+            "@Component(modules = TestModule.class)",
+            "interface TestComponent {",
+            // Test that we can request the raw type if it's provided by a module.
+            "  Foo foo();",
+            "}");
+    JavaFileObject foo =
+        JavaFileObjects.forSourceLines(
+            "test.Foo",
+            "package test;",
+            "",
+            "import jakarta.inject.Inject;",
+            "",
+            "class Foo<T> {",
+            "  @Inject Foo() {}",
+            "}");
+    JavaFileObject module =
+        JavaFileObjects.forSourceLines(
+            "test.TestModule",
+            "package test;",
+            "",
+            "import dagger.Module;",
+            "import dagger.Provides;",
+            "",
+            "@Module",
+            "class TestModule {",
+            // Test that Foo<T> can still be requested and is independent of Foo (otherwise we'd
+            // get a cyclic dependency error).
+            "  @Provides",
+            "  Foo provideFoo(Foo<Integer> fooInteger) {",
+            "    return fooInteger;",
+            "  }",
+            "",
+            "  @Provides",
+            "  int provideInt() {",
+            "    return 0;",
+            "  }",
+            "}");
+
+    Compilation compilation = daggerCompiler().compile(component, foo, module);
+    assertThat(compilation).succeeded();
+  }
+}
