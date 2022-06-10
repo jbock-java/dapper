9c0adab1e72001824044e1b9490e0d7e7e4bca0c
jbock <jbock-java@gmx.de>
2022-06-10 19:39:54 +0200

    add DelegateComponentProcessor

---
+++ compiler/src/main/java/dagger/internal/codegen/DelegateComponentProcessor.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2014 The Dagger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dagger.internal.codegen;
+
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.internal.codegen.base.ClearableCache;
+import dagger.internal.codegen.base.SourceFileGenerationException;
+import dagger.internal.codegen.base.SourceFileGenerator;
+import dagger.internal.codegen.binding.InjectBindingRegistry;
+import dagger.internal.codegen.binding.MembersInjectionBinding;
+import dagger.internal.codegen.binding.ProvisionBinding;
+import dagger.internal.codegen.bindinggraphvalidation.BindingGraphValidationModule;
+import dagger.internal.codegen.collect.ImmutableList;
+import dagger.internal.codegen.collect.ImmutableSet;
+import dagger.internal.codegen.componentgenerator.ComponentGeneratorModule;
+import dagger.internal.codegen.processingstep.ProcessingStepsModule;
+import dagger.internal.codegen.validation.BindingMethodValidatorsModule;
+import dagger.internal.codegen.validation.ExternalBindingGraphPlugins;
+import dagger.internal.codegen.validation.InjectBindingRegistryModule;
+import dagger.internal.codegen.validation.ValidationBindingGraphPlugins;
+import dagger.internal.codegen.xprocessing.XProcessingEnv;
+import dagger.internal.codegen.xprocessing.XProcessingEnvConfig;
+import dagger.internal.codegen.xprocessing.XProcessingStep;
+import dagger.internal.codegen.xprocessing.XRoundEnv;
+import dagger.spi.model.BindingGraphPlugin;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import java.util.Set;
+
+/** An implementation of Dagger's component processor that is shared between Javac and KSP. */
+final class DelegateComponentProcessor {
+  static final XProcessingEnvConfig PROCESSING_ENV_CONFIG =
+      new XProcessingEnvConfig.Builder().disableAnnotatedElementValidation(true).build();
+
+  @Inject InjectBindingRegistry injectBindingRegistry;
+  @Inject SourceFileGenerator<ProvisionBinding> factoryGenerator;
+  @Inject SourceFileGenerator<MembersInjectionBinding> membersInjectorGenerator;
+  @Inject ImmutableList<XProcessingStep> processingSteps;
+  @Inject ValidationBindingGraphPlugins validationBindingGraphPlugins;
+  @Inject ExternalBindingGraphPlugins externalBindingGraphPlugins;
+  @Inject Set<ClearableCache> clearableCaches;
+
+  // TODO(bcorso): Add support for external plugins with dagger.spi.model.BindingGraphPlugin
+  public void initialize(
+      XProcessingEnv env, ImmutableSet<BindingGraphPlugin> legacyExternalPlugins) {
+    DaggerDelegateComponentProcessor_Injector.factory()
+        .create(env, legacyExternalPlugins)
+        .inject(this);
+  }
+
+  public Iterable<XProcessingStep> processingSteps() {
+    validationBindingGraphPlugins.initializePlugins();
+    externalBindingGraphPlugins.initializePlugins();
+
+    return processingSteps;
+  }
+
+  public void postRound(XProcessingEnv env, XRoundEnv roundEnv) {
+    if (!roundEnv.isProcessingOver()) {
+      try {
+        injectBindingRegistry.generateSourcesForRequiredBindings(
+            factoryGenerator, membersInjectorGenerator);
+      } catch (SourceFileGenerationException e) {
+        e.printMessageTo(env.getMessager());
+      }
+    }
+    clearableCaches.forEach(ClearableCache::clearCache);
+  }
+
+  @Singleton
+  @Component(
+      modules = {
+        BindingGraphValidationModule.class,
+        BindingMethodValidatorsModule.class,
+        ComponentGeneratorModule.class,
+        InjectBindingRegistryModule.class,
+        ProcessingEnvironmentModule.class,
+        ProcessingRoundCacheModule.class,
+        ProcessingStepsModule.class,
+        SourceFileGeneratorsModule.class,
+      })
+  interface Injector {
+    void inject(DelegateComponentProcessor processor);
+
+    @Component.Factory
+    interface Factory {
+      Injector create(
+          @BindsInstance XProcessingEnv processingEnv,
+          @BindsInstance ImmutableSet<BindingGraphPlugin> legacyExternalPlugins);
+    }
+  }
+}
